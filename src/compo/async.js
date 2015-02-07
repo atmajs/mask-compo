4be8e15eb5244f88459c21fb7b920efb80fd9942
Alex Kit <source.kit@gmail.com>
2015-02-07 18:37:57 +0100

    * use Mask's Context Object

--- src/compo/async.js
+++ src/compo/async.js
@@ -1,84 +1,16 @@
 (function(){
-	
-	function _on(ctx, type, callback) {
-		if (ctx[type] == null)
-			ctx[type] = [];
-		
-		ctx[type].push(callback);
-		
-		return ctx;
-	}
-	
-	function _call(ctx, type, _arguments) {
-		var cbs = ctx[type];
-		if (cbs == null) 
-			return;
-		
-		for (var i = 0, x, imax = cbs.length; i < imax; i++){
-			x = cbs[i];
-			if (x == null)
-				continue;
-			
-			cbs[i] = null;
-			
-			if (_arguments == null) {
-				x();
-				continue;
-			}
-			
-			x.apply(this, _arguments);
-		}
-	}
-	
-	
-	var DeferProto = {
-		done: function(callback){
-			return _on(this, '_cbs_done', callback);
-		},
-		fail: function(callback){
-			return _on(this, '_cbs_fail', callback);
-		},
-		always: function(callback){
-			return _on(this, '_cbs_always', callback);
-		},
-		resolve: function(){
-			this.async = false;
-			_call(this, '_cbs_done', arguments);
-			_call(this, '_cbs_always', arguments);
-		},
-		reject: function(){
-			this.async = false;
-			_call(this, '_cbs_fail', arguments);
-			_call(this, '_cbs_always');
-		},
-		_cbs_done: null,
-		_cbs_fail: null,
-		_cbs_always: null
-	};
-	
-	var CompoProto = {
-		async: true,
-		await: function(resume){
-			this.resume = resume;
-		}
-	};
-	
 	Compo.pause = function(compo, ctx){
-		if (ctx.async == null) {
+		if (ctx.defers == null) 
 			ctx.defers = [];
-			obj_extend(ctx, DeferProto);
-		}
 		
 		ctx.async = true;
 		ctx.defers.push(compo);
 		
 		obj_extend(compo, CompoProto);
-		
 		return function(){
 			Compo.resume(compo, ctx);
 		};
 	};
-	
 	Compo.resume = function(compo, ctx){
 		
 		// fn can be null when calling resume synced after pause
@@ -105,4 +37,10 @@
 			ctx.resolve();
 	};
 	
+	var CompoProto = {
+		async: true,
+		await: function(resume){
+			this.resume = resume;
+		}
+	};
 }());
