9876e826bb810e726a118b461965d503a1a6e33a
Alex Kit <source.kit@gmail.com>
2015-11-20 16:24:30 +0100

    fix (Compo.pause) make ctx object unresolved

--- src/compo/async.js
+++ src/compo/async.js
@@ -10,8 +10,9 @@
 			}
 			ctx.async = true;
 			ctx.defers.push(compo);
+			ctx.defer();
 		}
-		
+
 		obj_extend(compo, CompoProto);
 		return function(){
 			Compo.resume(compo, ctx);
@@ -27,7 +28,7 @@
 		if (ctx == null) {
 			return;
 		}
-		
+
 		var busy = false,
 			dfrs = ctx.defers,
 			imax = dfrs.length,
@@ -35,17 +36,17 @@
 			x;
 		while ( ++i < imax ){
 			x = dfrs[i];
-			
+
 			if (x === compo) {
 				dfrs[i] = null;
 				continue;
 			}
 			busy = busy || x != null;
 		}
-		if (busy === false) 
+		if (busy === false)
 			ctx.resolve();
 	};
-	
+
 	var CompoProto = {
 		async: true,
 		await: function(resume){
