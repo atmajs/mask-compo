ff003c7ab87f472061bbf6e052e114e117cf46d5
Alex Kit <source.kit@gmail.com>
2016-04-25 11:45:45 +0200

    add (onRenderStart Async) support es6 promises

--- src/util/dfr.js
+++ src/util/dfr.js
@@ -1,18 +1,24 @@
 var dfr_isBusy;
 (function(){
 	dfr_isBusy = function(dfr){
-		if (dfr == null || typeof dfr.then !== 'function') 
+		if (dfr == null || typeof dfr.then !== 'function')
 			return false;
-		
+
 		// Class.Deferred
-		if (is_Function(dfr.isBusy)) 
+		if (is_Function(dfr.isBusy))
 			return dfr.isBusy();
-		
+
 		// jQuery Deferred
-		if (is_Function(dfr.state)) 
+		if (is_Function(dfr.state))
 			return dfr.state() === 'pending';
+
+		if (dfr instanceof Promise) {
+			return true;
+		}
 		
-		log_warn('Class or jQuery deferred interface expected');
+		log_warn('Class, jQuery or native promise expected');
 		return false;
 	};
+
+	var Promise = global.Promise;
 }());
